27c1f98381ec2ebd8d48a12342c600045431e02a
Chris Sidi <hashtagchrisdev@gmail.com>
2025-02-03 11:18:46 -0500

    Compare generated feed skeletons

---
+++ bluesky-feed-gen/test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+deno task generate-feed author-feed-01.json > skeleton-01.json
+deno task generate-feed author-feed-02.json > skeleton-02.json
+deno task generate-feed author-feed-03.json > skeleton-03.json
